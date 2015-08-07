cec63c91568d48d6b04453f4ded78ab643d0889a
Mario <mario.karlovcec@gmail.com>
2015-08-07 18:03:22 -0500

    html example

---
+++ buildingblocks/Simple web page HTML_CSS_JS/client.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+  $("#first_div").click(function(e){
+    alert("hi first box");
+  });
+
+  $("#second_div").click(function(e){
+    alert("hi second box");
+  });  
+
+});
